5d3f9d4aeff5d0348015f9c2fd2baf78bd0720cc
jagritisurabhi <jagritisurabhi17@gmail.com>
2024-02-29 12:58:36 +0530

    Added solutions for 04, 05, 06, 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+    let sum = 0;
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
